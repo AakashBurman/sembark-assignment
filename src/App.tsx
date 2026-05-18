e56ad445040254e368809de765ab9c2b3af98ec7
Aakash Burman <burmanaakash786@gmail.com>
2026-05-18 21:01:08 +0530

    setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import CartPage from "./pages/CartPage";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/product/:id" element={<ProductDetailPage />} />
+      <Route path="/cart" element={<CartPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  );
 };
 
 export default App;
