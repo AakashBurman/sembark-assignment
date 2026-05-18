1485576d746ce22b32d35459d05b106aef097336
Aakash Burman <burmanaakash786@gmail.com>
2026-05-18 21:44:46 +0530

    setup main-layout & app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import MainLayout from "./layouts/MainLayout";
 import CartPage from "./pages/CartPage";
 import HomePage from "./pages/HomePage";
 import NotFoundPage from "./pages/NotFoundPage";
@@ -8,9 +9,11 @@ import ProductDetailPage from "./pages/ProductDetailPage";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/product/:id" element={<ProductDetailPage />} />
-      <Route path="/cart" element={<CartPage />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
